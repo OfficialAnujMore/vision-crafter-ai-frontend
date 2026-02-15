f55f5b213a3be8682e360d7b87478d90e0d88c32
Anuj C More <moreanuj1307@gmail.com>
2026-02-15 11:20:02 -0800

    Displayed Strict Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
@@ -7,9 +7,9 @@ import App from './App.tsx'
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
-  </StrictMode>,
+  // </StrictMode>,
 )
